dd96ad23adf287c86eea99649980b87b46426b1a
oldflag <oldflag2333333@gmail.com>
2024-05-01 18:40:56 +0800

    fix

--- lua/oldvim/plugins/format.lua
+++ lua/oldvim/plugins/format.lua
@@ -9,6 +9,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       nix = { 'nixfmt' },
+      css = { 'rustywind' },
       -- sh = { 'shfmt' },
       -- Conform can also run multiple formatters sequentially
       --
