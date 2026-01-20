e4df3d60f91c42bf0810ee0b2a13351c67157407
oldflag <oldflag2333333@gmail.com>
2026-01-20 17:21:48 +0800

    fix treesitter

--- lua/oldvim/plugins/treesitter.lua
+++ lua/oldvim/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     enabled = true,
+    build = ':TSUpdate',
     init = function(plugin)
       require('lazy.core.loader').add_to_rtp(plugin)
     end,
